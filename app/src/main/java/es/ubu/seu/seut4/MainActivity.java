9257a63c89348803e34f2b7637cdf69b5f478c0f
rodrimmbdev <rodrimmb@gmail.com>
2017-04-15 20:24:11 +0200

    Resuelto el ejercicio usando la libreria Butter Knife

--- app/src/main/java/es/ubu/seu/seut4/MainActivity.java
+++ app/src/main/java/es/ubu/seu/seut4/MainActivity.java
@@ -4,18 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_label) TextView label;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        changeTextOfLabel();
-    }
-
-    private void changeTextOfLabel() {
-        TextView label = (TextView) findViewById(R.id.main_label);
-        label.setText("Titulo de la app");
+        ButterKnife.bind(this);
+        label.setText(R.string.exercise_title);
     }
 }
