d0cda49bc82e44391f0a353dc6313e2bf837fd48
rodrimmbdev <rodrimmb@gmail.com>
2017-04-18 22:12:13 +0200

    Ejercicio 4 resuelto con outer class

--- app/src/main/java/es/ubu/seu/seut4/MainActivity.java
+++ app/src/main/java/es/ubu/seu/seut4/MainActivity.java
@@ -2,6 +2,9 @@ package es.ubu.seu.seut4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -10,6 +13,9 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.main_label) TextView label;
+    @BindView(R.id.btn_submit) Button btnSubmit;
+    @BindView(R.id.btn_cancel) Button btnCancel;
+    @BindView(R.id.input_text) EditText inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         label.setText(R.string.exercise_title);
+
+        View.OnClickListener clickHandler = new MainButtonsHandler(getApplicationContext(), inputText);
+
+        btnSubmit.setOnClickListener(clickHandler);
+        btnCancel.setOnClickListener(clickHandler);
     }
 }
