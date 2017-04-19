71bbf9bcf44789fb1f5ddb448761b37a40da507c
rodrimmbdev <rodrimmb@gmail.com>
2017-04-19 23:16:05 +0200

    Ejercicio 5 resuelto, creada lista simple

--- app/src/main/java/es/ubu/seu/seut4/SecondActivity.java
+++ app/src/main/java/es/ubu/seu/seut4/SecondActivity.java
@@ -2,12 +2,22 @@ package es.ubu.seu.seut4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SecondActivity extends AppCompatActivity {
+
+    @BindView(R.id.elements_list) ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        ButterKnife.bind(this);
+        String[] elements = {"Elemento 1","Elemento 2","Elemento 3","Elemento 4"};
+        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, elements));
     }
 }
