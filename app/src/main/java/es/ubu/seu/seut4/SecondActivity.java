b8ada8c6f4117474faef28d2a0db62de25fe00e3
rodrimmbdev <rodrimmb@gmail.com>
2017-04-24 18:09:37 +0200

    Ejercicio 6 boton para añadir nuevos usuarios a la lista en tiempo de ejecucion.

--- app/src/main/java/es/ubu/seu/seut4/SecondActivity.java
+++ app/src/main/java/es/ubu/seu/seut4/SecondActivity.java
@@ -2,18 +2,23 @@ package es.ubu.seu.seut4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import es.ubu.seu.seut4.adapters.UserArrayAdapterFactory;
+import es.ubu.seu.seut4.model.User;
 import es.ubu.seu.seut4.services.UserService;
 
 public class SecondActivity extends AppCompatActivity {
 
     @BindView(R.id.elements_list) ListView listView;
+    @BindView(R.id.btn_add_user) Button addUser;
 
-    private UserService userService;
+    protected UserService userService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +28,35 @@ public class SecondActivity extends AppCompatActivity {
 
         userService = new UserService();
         listView.setAdapter(
-                UserArrayAdapterFactory.getArrayAdapterWithAndroidNative(
+                UserArrayAdapterFactory.getArrayAdapterWithButterKnife(
                         getApplicationContext(),
                         userService.getAllUsers()
                 )
         );
+        addUser.setOnClickListener(new AddUserClickHandler());
+    }
+
+    private class AddUserClickHandler implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.btn_add_user) {
+                ArrayAdapter<User> adapter = (ArrayAdapter) listView.getAdapter();
+                addNewUser(new Integer(listView.getCount()));
+                adapter.notifyDataSetChanged();
+            }
+        }
+
+        private void addNewUser(Integer numberOfElements){
+            userService.addUser(
+                    new User(
+                            Long.getLong(numberOfElements.toString()),
+                            "Nombre"+numberOfElements,
+                            "Apellidos"+numberOfElements,
+                            "Alumno",
+                            "Universidad de Burgos"
+                    )
+            );
+        }
     }
 }
--- app/src/main/java/es/ubu/seu/seut4/services/UserService.java
+++ app/src/main/java/es/ubu/seu/seut4/services/UserService.java
@@ -1,5 +1,6 @@
 package es.ubu.seu.seut4.services;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,17 +12,23 @@ import es.ubu.seu.seut4.model.User;
 
 public class UserService {
 
-    private List<User> users;
+    private List<User> users = new ArrayList<User>();
 
     public UserService(){
-        users = Arrays.asList(new User[]{
-                new User(1L, "Raul", "Marticorena Sanchez", "Profesor titular", "Universidad de Burgos"),
-                new User(2L, "Rodrigo", "Manjon Martin", "Profesor asociado", "TuLotero"),
-                new User(3L, "Alejandro", "Merino Gomez", "Profesor titular", "Universidad de Burgos")
-        });
+        users.addAll(Arrays.asList(
+                new User[]{
+                    new User(1L, "Raul", "Marticorena Sanchez", "Profesor titular", "Universidad de Burgos"),
+                    new User(2L, "Rodrigo", "Manjon Martin", "Profesor asociado", "TuLotero"),
+                    new User(3L, "Alejandro", "Merino Gomez", "Profesor titular", "Universidad de Burgos")
+                }
+        ));
     }
 
     public List<User> getAllUsers() {
         return users;
+    }
+
+    public void addUser(User user) {
+        users.add(user);
     }
 }
